636fbddd268e28cc74497e10a1839e03662ef640
RasCarlito <rascarlito@gmail.com>
2017-07-21 16:46:18 +0200

    Override UserController.me to add populated values

---
+++ api/controllers/UserController.js
@@ -0,0 +1,26 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing timeentries
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+/* globals User */
+
+const get = require('lodash/get')
+const merge = require('lodash/merge')
+const _super = require('sails-auth/api/controllers/UserController')
+
+const controller = {
+  me (req, res) {
+    const id = get(req.session, 'user.id')
+
+    if (!id) return res.ok(null)
+
+    User.findOne({ id }).populate('settings').populate('timeEntries')
+      .then(user => res.ok(user))
+      .catch(() => res.ok(null))
+  }
+}
+
+module.exports = merge(_super, controller)
